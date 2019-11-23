7b4995305e07bdde551828e9f0962ae3c2e77a3d
GaikwadRavi03 <gaikwadr576@gmail.com>
2019-11-22 19:16:12 -0500

    Add: Check valid and invalid email id

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -6,8 +6,8 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 
     public static boolean userFirstName(String firstName) {
-        Pattern firstNamePattern= Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
-        Matcher match=firstNamePattern.matcher(firstName);
+        Pattern firstNamePattern = Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
+        Matcher match = firstNamePattern.matcher(firstName);
         if (match.matches())
             return true;
         else
@@ -15,11 +15,22 @@ public class UserRegistration {
     }
 
     public static boolean userLastName(String lastName) {
-        Pattern lastNamePattern= Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
-        Matcher match=lastNamePattern.matcher(lastName);
+        Pattern lastNamePattern = Pattern.compile("^[A-Z]{1}[a-z]{3,}$");
+        Matcher match = lastNamePattern.matcher(lastName);
+        if (match.matches())
+            return true;
+        else
+            return false;
+    }
+
+
+    public Boolean validateUserEmail(String userEmail) {
+        Pattern lastNamePattern = Pattern.compile("^[a-zA-Z0-9]([._+]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2}){0,1}$");
+        Matcher match = lastNamePattern.matcher(userEmail);
         if (match.matches())
             return true;
         else
             return false;
     }
 }
+
--- src/test/java/com/userregistration/UserRegistrationTest.java
+++ src/test/java/com/userregistration/UserRegistrationTest.java
@@ -5,20 +5,24 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
 
-        @Test
-        public void givenMessageShowTrueWhenFirstNameWithCapitalLetter() {
-            Assert.assertTrue(UserRegistration.userFirstName("Ravi"));
-        }
-        @Test
-        public void givenMessageShowFalseWhenFirstNameWithoutCapitalLetterAndWithSymbols() {
-            Assert.assertTrue(UserRegistration.userFirstName("ravi@"));
-        }
-        @Test
-        public void givenMessageShowTrueWhenLastNameWithCapitalLetter() {
-            Assert.assertTrue(UserRegistration.userLastName("Gaikwad"));
-        }
-        @Test
-        public void givenMessageShowFalseWhenLastNameWithoutCapitalLetterAndWithSymbols() {
-            Assert.assertTrue(UserRegistration.userLastName("g$aikwad#"));
-        }
+    @Test
+    public void givenMessageShowTrueWhenFirstNameWithCapitalLetter() {
+        Assert.assertTrue(UserRegistration.userFirstName("Ravi"));
+    }
+
+    @Test
+    public void givenMessageShowFalseWhenFirstNameWithoutCapitalLetterAndWithSymbols() {
+        Assert.assertTrue(UserRegistration.userFirstName("ravi@"));
+    }
+
+    @Test
+    public void givenMessageShowTrueWhenLastNameWithCapitalLetter() {
+        Assert.assertTrue(UserRegistration.userLastName("Gaikwad"));
+    }
+
+    @Test
+    public void givenMessageShowFalseWhenLastNameWithoutCapitalLetterAndWithSymbols() {
+        Assert.assertTrue(UserRegistration.userLastName("g$aikwad#"));
+    }
+
 }
